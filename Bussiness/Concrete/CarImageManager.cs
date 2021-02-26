25b9ffbfea1deabc1294940a7cd7ab0bbda27ddf
Çağlar Demir <cagdem2@gmail.com>
2021-02-26 07:10:12 +0300

    13. gün ödev 1 commit 1

---
+++ Bussiness/Concrete/CarImageManager.cs
@@ -0,0 +1,42 @@
+﻿using Bussiness.Abstract;
+using Core.Utilities.Results;
+using DataAccess.Abstract;
+using Entities.Concrete;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Bussiness.Concrete
+{
+    public class CarImageManager : ICarImageService
+    {
+        ICarImageDal _carImageDal;
+
+        public CarImageManager(ICarImageDal carImageDal)
+        {
+            _carImageDal = carImageDal;
+        }
+        public IResult Add(CarImage carImage)
+        {
+            _carImageDal.Add(carImage);
+            return new SuccessResult();
+        }
+
+        public IResult Delete(CarImage carImage)
+        {
+            _carImageDal.Delete(carImage);
+            return new SuccessResult();
+        }
+
+        public IDataResult<List<CarImage>> List(int id)
+        {
+            return new SuccessDataResult<List<CarImage>>(_carImageDal.GetAll(c=>c.CarId == id));
+        }
+
+        public IResult Update(CarImage carImage)
+        {
+            _carImageDal.Update(carImage);
+            return new SuccessResult();
+        }
+    }
+}
---
+++ WebAPI/Controllers/CarImagesController.cs
@@ -0,0 +1,58 @@
+﻿using Bussiness.Abstract;
+using Entities.Concrete;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Threading.Tasks;
+using WebAPI.Models;
+
+namespace WebAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class CarImagesController : ControllerBase
+    {
+        public static IWebHostEnvironment _webHostEnvironment;
+        ICarImageService _carImageService;
+
+        public CarImagesController(IWebHostEnvironment webHostEnvironment, ICarImageService carImageService)
+        {
+            _carImageService = carImageService;
+            _webHostEnvironment = webHostEnvironment;
+        }
+
+
+        [HttpPost("add")]
+        public IActionResult Add([FromForm] FileUpload image, [FromForm] CarImage carImage)
+        {
+            string path = _webHostEnvironment.WebRootPath + "\\uploads\\";
+
+            if (image.files.Length > 0)
+            {
+                if (!Directory.Exists(path))
+                {
+                    Directory.CreateDirectory(path);
+                }
+                using (FileStream fileStream = System.IO.File.Create(path + image.files.FileName))
+                {
+                    image.files.CopyTo(fileStream);
+                    fileStream.Flush();
+                }
+            }
+            var tempCarImage = new CarImage { Id= carImage.Id, CarId = carImage.CarId, ImagePath = path, Date = DateTime.Now };
+            var result = _carImageService.Add(tempCarImage);
+            
+            if (result.Success)
+            {
+                return Ok();
+            }
+
+            return BadRequest();
+
+        }
+    }
+}
